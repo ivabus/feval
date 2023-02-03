aa996ec3efaba255cb30a7b8c81f4b89b0e7fceb
Ivan Bushchik <ivabus@ivabus.dev>
2023-02-03 15:45:34 +0300

    tasks.rs: add exit function

--- src/tasks.rs
+++ src/tasks.rs
@@ -2,9 +2,15 @@ pub mod tasks {
     use std::f64::consts;
 
 	pub fn all(expr: String) -> String {
-		convert_constants(expr)
+		convert_constants(exit(expr))
 	}
 	fn convert_constants(expr: String) -> String {
 		expr.replace("math::pi", &consts::PI.to_string()).replace("math::e", &consts::E.to_string())
+	}
+	fn exit(expr: String) -> String {
+		if expr.contains("exit") {
+			std::process::exit(0);
+		}
+		expr
 	}
 }
